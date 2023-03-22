8c317db845b69a02b701350d0273ed225089e6d9
URiem <uriemenschneider@gmail.com>
2023-03-22 15:37:36 +0000

    Add loop around questions, extract index of correct answer, add favicon

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,7 @@
 const allQuestions = {
     'What is Canada\'s Capital?' : ['Toronto', 'Vancuver', 'Ottowa', 'Quebec', 2],
     
-    'What is Germany\s Capital?' : ['Berlin', 'Bonn' , 'Baden-Baden', 'Munich', 0],
+    'What is Germany\'s Capital?' : ['Berlin', 'Bonn' , 'Baden-Baden', 'Munich', 0],
     
     'What is the capital of Jamaica? ' : ['Montego Bay', 'Port Antonio', 'Santa Cruz', 'Kingston', 3]
   };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 let curr = 0;
 
+let numOfQs = Object.keys(allQuestions).length;
+
+for (i = 0; i < numOfQs; i++) {
+    curr = i;
 let question = Object.keys(allQuestions)[curr];
 let answers = Object.values(allQuestions)[curr];
 let correctAnswer = answers[answers.length-1];
 console.log(question);
 console.log(answers);
 console.log(correctAnswer);
+console.log(numOfQs);
 
 document.getElementById("question-text").textContent = question;
 document.getElementById("a").textContent = answers[0];
 document.getElementById("b").textContent = answers[1];
 document.getElementById("c").textContent = answers[2];
 document.getElementById("d").textContent = answers[3];
+}
