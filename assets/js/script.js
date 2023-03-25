48acfaaae3e1cfb1119665ab47575b4442d16744
URiem <uriemenschneider@gmail.com>
2023-03-25 10:41:37 +0000

    Convert list items with answers to buttons and add eventlisteners to listen for button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "answer1") {
+                alert("You have chosen answer 1!")
+            } else if (this.getAttribute("data-type") === "answer2") {
+                alert("You have chosen answer 2!")
+            } else if (this.getAttribute("data-type") === "answer3") {
+                alert("You have chosen anser 3!")
+            } else if (this.getAttribute("data-type") === "answer4") {
+                alert("You have chosen answer 4!")
+            } else {
+                alert("You have chosen and invalid answer!")
+            }
+
+        })
+
+    }
+
+})
+
 let curr = 0;
 
 let numOfQs = allQuestions.length;
@@ -14,22 +40,22 @@ for (i = 0; i < numOfQs; i++) {
     document.getElementById("correct-answer").textContent = currentQuestion.correctAns;
 }
 
-function runGame {
+// function runGame {
 
-}
+// }
 
-function checkAnswer {
+// function checkAnswer {
 
-}
+// }
 
-function incrementScore {
+// function incrementScore {
 
-}
+// }
 
-function incrementWrongAnswer {
+// function incrementWrongAnswer {
 
-}
+// }
 
-function displayQuestion {
-    
-}
+// function displayQuestion {
+
+// }
