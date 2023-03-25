f4ea61487891f026a84b692a2d67eeebe1000005
URiem <uriemenschneider@gmail.com>
2023-03-25 09:53:12 +0000

    Change structure of questions from object to array following mentor Brian's suggestion

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,27 @@
-const allQuestions = {
-    'What is Canada\'s Capital?' : ['Toronto', 'Vancuver', 'Ottowa', 'Quebec', 2],
+// const allQuestions = {
+//     'What is Canada\'s Capital?' : ['Toronto', 'Vancuver', 'Ottowa', 'Quebec', 2],
     
-    'What is Germany\'s Capital?' : ['Berlin', 'Bonn' , 'Baden-Baden', 'Munich', 0],
+//     'What is Germany\'s Capital?' : ['Berlin', 'Bonn' , 'Baden-Baden', 'Munich', 0],
     
-    'What is the capital of Jamaica? ' : ['Montego Bay', 'Port Antonio', 'Santa Cruz', 'Kingston', 3]
-  };
+//     'What is the capital of Jamaica? ' : ['Montego Bay', 'Port Antonio', 'Santa Cruz', 'Kingston', 3]
+//   };
+
+  const allQuestions = [
+    {
+      question: 'What is Canadas Capital?',
+      answers: ['Toronto', 'Vancouver', 'Ottowa', 'Quebec'],
+      correctAns: 'Ottowa'
+    },
+    {
+      question: 'What is Germanys Captial?',
+      answers: ['Berlin', 'Bonn' , 'Baden-Baden', 'Munich'],
+      correctAns: 'Berlin'
+    },
+    {
+      question: 'What is the capital of Jamaica?',
+      answers: ['Montego Bay', 'Port Antonio', 'Santa Cruz', 'Kingston'],
+      correctAns: 'Kingston'
+    }
+  ]
+  
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,15 @@
 let curr = 0;
 
-let numOfQs = Object.keys(allQuestions).length;
+let numOfQs = allQuestions.length;
+console.log(numOfQs);
 
 for (i = 0; i < numOfQs; i++) {
     curr = i;
-let question = Object.keys(allQuestions)[curr];
-let answers = Object.values(allQuestions)[curr];
-let correctAnswer = answers[answers.length-1];
-console.log(question);
-console.log(answers);
-console.log(correctAnswer);
-console.log(numOfQs);
-
-document.getElementById("question-text").textContent = question;
-document.getElementById("a").textContent = answers[0];
-document.getElementById("b").textContent = answers[1];
-document.getElementById("c").textContent = answers[2];
-document.getElementById("d").textContent = answers[3];
-document.getElementById("correct-answer").textContent = answers[answers[4]];
+    currentQuestion = allQuestions[i];
+    document.getElementById("question-text").textContent = currentQuestion.question;
+    document.getElementById("a").textContent = currentQuestion.answers[0];
+    document.getElementById("b").textContent = currentQuestion.answers[1];
+    document.getElementById("c").textContent = currentQuestion.answers[2];
+    document.getElementById("d").textContent = currentQuestion.answers[3];
+    document.getElementById("correct-answer").textContent = currentQuestion.correctAns;
 }
