08d62061121337ae9bc438f08fc7e480443d05af
URiem <uriemenschneider@gmail.com>
2023-03-27 17:32:08 +0000

    Complete set of 10 easy questions. Add set of 10 hard questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,16 +1,87 @@
  // An array that holds all the questions and possible answers, including the corrent answer.
 const allQuestions = [
     {
-      question: 'What is Canadas Capital?',
+      question: 'What is the captial of Canada?',
       answers: ['Toronto', 'Vancouver', 'Ottowa', 'Quebec', 2],
     },
     {
-      question: 'What is Germanys Captial?',
+      question: 'What is the capital of Germany?',
       answers: ['Berlin', 'Bonn' , 'Baden-Baden', 'Munich', 0]
     },
     {
       question: 'What is the capital of Jamaica?',
       answers: ['Montego Bay', 'Port Antonio', 'Santa Cruz', 'Kingston', 3]
+    },
+    {
+      question: 'What is the capital of Norway?',
+      answers: ['Bergen', 'Tromso' , 'Trondheim', 'Oslo', 3]
+    },
+    {
+      question: 'What is the capital of Italy?',
+      answers: ['Naples', 'Rome' , 'Milan', 'Palermo', 1]
+    },
+    {
+      question: 'What is the capital of Japan?',
+      answers: ['Tokyo', 'Kyoto' , 'Saporo', 'Osaka', 0]
+    },
+    {
+      question: 'What is the capital of the United States of America?',
+      answers: ['New York', 'Los Angeles' , 'Washington DC', 'Boston', 2]
+    },
+    {
+      question: 'What is the capital of Ireland?',
+      answers: ['Cork', 'Galway' , 'Waterford', 'Dublin', 3]
+    },
+    {
+      question: 'What is the capital of Russia?',
+      answers: ['Moskow', 'Saint Petersburg' , 'Vladivostok', 'Kazan', 0]
+    },
+    {
+      question: 'What is the capital of New Zealand?',
+      answers: ['Christchurch', 'Auckland' , 'Wellington', 'Hobbiton', 2]
+    }
+  ]
+
+  const = allQuestionsHard [
+    {
+      question: 'What is the capital of Lichtenstein?',
+      answers: ['Vaduz', 'Schaan' , 'Triesen', 'Balzers', 0]
+    },
+    {
+      question: 'What is the capital of Lesotho?',
+      answers: ['Thaba-Tseka', 'Maseru' , 'Mafeteng', 'Malealea', 1]
+    },
+    {
+      question: 'What is the capital of Burkina Faso?',
+      answers: ['Koudougou', 'Bobo-Dioulasso' , 'Banfora', 'Ouagadougou', 3]
+    },
+    {
+      question: 'What is the capital of Tuvalu?',
+      answers: ['Vaiaku', 'Lolua' , 'Funafuti', 'Asau', 2]
+    },
+    {
+      question: 'What is the capital of Tanzania?',
+      answers: ['Dar Es Salaam', 'Dodoma' , 'Tanga', 'Morgoro', 1]
+    },
+    {
+      question: 'What is the capital of Myanmar?',
+      answers: ['Mandalay', 'Yangon' , 'Naypyidaw', 'Sagaing', 2]
+    },
+    {
+      question: 'What is the capital of Morocco?',
+      answers: ['Rabat', 'Marrakesh' , 'Casalanca', 'Tangier', 0]
+    },
+    {
+      question: 'What is the capital of Italy?',
+      answers: ['Naples', 'Rome' , 'Milan', 'Palermo', 1]
+    },
+    {
+      question: 'What is the capital of Italy?',
+      answers: ['Naples', 'Rome' , 'Milan', 'Palermo', 1]
+    },
+    {
+      question: 'What is the capital of Italy?',
+      answers: ['Naples', 'Rome' , 'Milan', 'Palermo', 1]
     }
   ]
   
