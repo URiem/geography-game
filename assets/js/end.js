f412a49ddee194da551fe41c32bd04c96ba3a0f0
URiem <uriemenschneider@gmail.com>
2023-03-28 15:26:49 +0000

    Start work on creating an array which saves the levels and acchieved scores

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,3 +3,14 @@ let mostRecentScore = localStorage.getItem("mostRecentScore");
 let userLevel = localStorage.getItem("userLevel");
 
 finalScore.innerText = `${mostRecentScore} @ level  ${userLevel}` ;
+
+// Save final score and level of each game in an array
+
+const score = [
+    { 
+    level: userLevel,
+    score: finalScore
+    }
+]
+
+console.log(score);
