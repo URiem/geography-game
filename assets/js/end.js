43ce2852a718b83826af788fdc002f7a387ae58b
URiem <uriemenschneider@gmail.com>
2023-03-29 13:33:36 +0000

    Comment out currently unused code

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,16 +1,28 @@
 let finalScore = document.getElementById('final-score');
 let mostRecentScore = localStorage.getItem("mostRecentScore");
 let userLevel = localStorage.getItem("userLevel");
+// let scoreLog = localStorage.getItem("scoreLog");
 
-finalScore.innerText = `${mostRecentScore} @ level  ${userLevel}` ;
+finalScore.innerText = `${mostRecentScore} @ ${userLevel} Level` ;
+
+// Save the username
+
+// function saveUserName() {
+//     userName = document.getElementById('username').textContent;
+// }
+
+// console.log(userName);
 
 // Save final score and level of each game in an array
+    const score = [
+        { 
+        level: userLevel,
+        score: mostRecentScore,
+        }
+    ]
 
-const score = [
-    { 
-    level: userLevel,
-    score: finalScore
-    }
-]
+    // localStorage.setItem("ScoreLog",score);
+    // scoreLog.push(score)
+
 
 console.log(score);
