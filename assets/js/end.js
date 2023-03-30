c5c58e74e91fede23b67740efc5621a907de17d4
URiem <uriemenschneider@gmail.com>
2023-03-30 10:36:45 +0000

    Add end of game score and level based message  using an if else if statement

--- assets/js/end.js
+++ assets/js/end.js
@@ -5,6 +5,34 @@ let userLevel = localStorage.getItem("userLevel");
 
 finalScore.innerText = `${mostRecentScore} @ ${userLevel} Level` ;
 
+if (userLevel == 'Easy' && mostRecentScore < 4) {
+    document.getElementById('final-message').innerText = "Start Exploring!! There is so much more to learn!";
+} else if (userLevel == 'Easy' && mostRecentScore < 8) {
+    document.getElementById('final-message').innerText = "Nice work! See if you can improve!";
+} else if (userLevel == 'Easy' && mostRecentScore <= 10) {
+    document.getElementById('final-message').innerText = "Excellent result at the easy level! Why not try the next level?";
+} else if (userLevel == 'Intermediate' && mostRecentScore < 4) {
+    document.getElementById('final-message').innerText = "Good progress!! Keep learning!";
+} else if (userLevel == 'Intermediate' && mostRecentScore < 8) {
+    document.getElementById('final-message').innerText = "Nice work! See if you can improve!";
+} else if (userLevel == 'Intermadiate' && mostRecentScore <= 10) {
+    document.getElementById('final-message').innerText = "Excellent result at the intermediate level! Try the next level?";
+} else if (userLevel == 'Advanced' && mostRecentScore < 4) {
+    document.getElementById('final-message').innerText = "Good job!! Keep learning!";
+} else if (userLevel == 'Advanced' && mostRecentScore < 8) {
+    document.getElementById('final-message').innerText = "Check out a map! See if you can figure out the answers!";
+} else if (userLevel == 'Advanced' && mostRecentScore <= 10) {
+    document.getElementById('final-message').innerText = "Excellent result at the advanced level! Are you ready to be an expert?";
+} else if (userLevel == 'Expert' && mostRecentScore < 4) {
+    document.getElementById('final-message').innerText = "Good job!! Keep learning!";
+} else if (userLevel == 'Expert' && mostRecentScore < 8) {
+    document.getElementById('final-message').innerText = "Wow!! You probably love studying maps!!!";
+} else if (userLevel == 'Expert' && mostRecentScore <= 10) {
+    document.getElementById('final-message').innerText = "Amazing!! Do you know the capitals of the remaining 155 countries aswell?";
+} else {
+    document.getElementById('final-message').innerText = "Great work!";
+}
+
 // Save the username
 
 // function saveUserName() {
@@ -14,15 +42,15 @@ finalScore.innerText = `${mostRecentScore} @ ${userLevel} Level` ;
 // console.log(userName);
 
 // Save final score and level of each game in an array
-    const score = [
-        { 
-        level: userLevel,
-        score: mostRecentScore,
-        }
-    ]
+    // const score = [
+    //     { 
+    //     level: userLevel,
+    //     score: mostRecentScore,
+    //     }
+    // ]
 
     // localStorage.setItem("ScoreLog",score);
     // scoreLog.push(score)
 
 
-console.log(score);
+// console.log(score);
