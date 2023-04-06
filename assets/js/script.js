c8ad210b0d281cc9d50669771a25a3ea69cc6cf0
URiem <uriemenschneider@gmail.com>
2023-04-06 14:11:36 +0000

    Reposition validate function in script.js based on JShint testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,34 +19,35 @@ if (window.localStorage.getItem("username")) {
     const submit = document.getElementById("submit");
     submit.addEventListener('click', validate);
 
-    /**
-     * This function validates that a username was input. It shows an error message if not.
-     * If a username is provided it saves it so it can be displayed throughout the game.
-     * Adapted from https://www.freecodecamp.org/news/form-validation-with-html5-and-javascript/
-     * @param {event} e - event of a click
-     */
-    function validate(e) {
-        e.preventDefault();
+}
 
-        const usernameField = document.getElementById("username");
+/**
+ * This function validates that a username was input. It shows an error message if not.
+ * If a username is provided it saves it so it can be displayed throughout the game.
+ * Adapted from https://www.freecodecamp.org/news/form-validation-with-html5-and-javascript/
+ * @param {event} e - event of a click
+ */
+function validate(e) {
+    e.preventDefault();
 
-        if (!usernameField.value) {
-            const usernameError = document.getElementById("usernameError");
-            usernameError.classList.add("visible");
-            usernameError.setAttribute('aria-hidden', false);
-            usernameError.setAttribute('aria-invalid', true);
-            usernameError.innerText = "Please enter a username to proceed!";
-        } else if (usernameField.value) {
-            window.localStorage.setItem("username", usernameField.value);
-            document.getElementById("levelchoice-area").style.display = "initial";
-            document.getElementById("username-area").style.display = "none";
-            window.localStorage.setItem("username", usernameField.value);
-        }
+    const usernameField = document.getElementById("username");
 
-        let user = window.localStorage.getItem("username");
-        document.getElementById("levelchoice-heading").innerText = `Welcome to the game ${user}!`;
-
+    if (!usernameField.value) {
+        const usernameError = document.getElementById("usernameError");
+        usernameError.classList.add("visible");
+        usernameError.setAttribute('aria-hidden', false);
+        usernameError.setAttribute('aria-invalid', true);
+        usernameError.innerText = "Please enter a username to proceed!";
+    } else if (usernameField.value) {
+        window.localStorage.setItem("username", usernameField.value);
+        document.getElementById("levelchoice-area").style.display = "initial";
+        document.getElementById("username-area").style.display = "none";
+        window.localStorage.setItem("username", usernameField.value);
     }
+
+    let user = window.localStorage.getItem("username");
+    document.getElementById("levelchoice-heading").innerText = `Welcome to the game ${user}!`;
+
 }
 
 let questionArea = document.getElementById('question-area');
